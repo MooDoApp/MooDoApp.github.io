7a2053bfe22958379f47f95d1d967a06d8482f2d
Jay Meistrich <jmeistrich@gmail.com>
2018-05-27 16:53:26 +0700

    Release - 1527414530090

---
+++ react-static-routes.js
@@ -0,0 +1,118 @@
+
+
+import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
+
+import universal, { setHasBabelPlugin } from 'react-universal-component'
+
+import { cleanPath } from 'react-static'
+
+
+
+setHasBabelPlugin()
+
+const universalOptions = {
+  loading: () => null,
+  error: props => {
+    console.error(props.error);
+    return <div>An error occurred loading this page's template. More information is available in the console.</div>;
+  },
+}
+
+  const t_0 = universal(import('../src/containers/Home'), universalOptions)
+const t_1 = universal(import('../src/containers/Premium'), universalOptions)
+const t_2 = universal(import('../src/containers/Help'), universalOptions)
+const t_3 = universal(import('../src/containers/Us'), universalOptions)
+const t_4 = universal(import('../src/containers/Contact'), universalOptions)
+const t_5 = universal(import('../src/containers/Press'), universalOptions)
+const t_6 = universal(import('../src/containers/Privacy'), universalOptions)
+const t_7 = universal(import('../src/containers/Unsubscribe'), universalOptions)
+const t_8 = universal(import('../src/containers/Purchase'), universalOptions)
+const t_9 = universal(import('../src/containers/Invoice'), universalOptions)
+const t_10 = universal(import('../src/containers/Beta'), universalOptions)
+const t_11 = universal(import('../src/containers/Blog'), universalOptions)
+const t_12 = universal(import('../src/containers/404'), universalOptions)
+
+
+// Template Map
+global.componentsByTemplateID = global.componentsByTemplateID || [
+  t_0,
+t_1,
+t_2,
+t_3,
+t_4,
+t_5,
+t_6,
+t_7,
+t_8,
+t_9,
+t_10,
+t_11,
+t_12
+]
+
+// Template Tree
+global.templateIDsByPath = global.templateIDsByPath || {
+  '404': 12
+}
+
+// Get template for given path
+const getComponentForPath = path => {
+  path = cleanPath(path)
+  return global.componentsByTemplateID[global.templateIDsByPath[path]]
+}
+
+global.reactStaticGetComponentForPath = getComponentForPath
+global.reactStaticRegisterTemplateIDForPath = (path, id) => {
+  global.templateIDsByPath[path] = id
+}
+
+export default class Routes extends Component {
+  render () {
+    const { component: Comp, render, children } = this.props
+
+    const getFullComponentForPath = path => {
+      let Comp = getComponentForPath(path)
+      let is404 = path === '404'
+      if (!Comp) {
+        is404 = true
+        Comp = getComponentForPath('404')
+      }
+      return newProps => (
+        Comp
+          ? <Comp {...newProps} {...(is404 ? {is404: true} : {})} />
+          : null
+      )
+    }
+
+    const renderProps = {
+      componentsByTemplateID: global.componentsByTemplateID,
+      templateIDsByPath: global.templateIDsByPath,
+      getComponentForPath: getFullComponentForPath
+    }
+
+    if (Comp) {
+      return (
+        <Comp
+          {...renderProps}
+        />
+      )
+    }
+
+    if (render || children) {
+      return (render || children)(renderProps)
+    }
+
+    // This is the default auto-routing renderer
+    return (
+      <Route path='*' render={props => {
+        let Comp = getFullComponentForPath(props.location.pathname)
+        // If Comp is used as a component here, it triggers React to re-mount the entire
+        // component tree underneath during reconciliation, losing all internal state.
+        // By unwrapping it here we keep the real, static component exposed directly to React.
+        return Comp && Comp({ ...props, key: props.location.pathname })
+      }} />
+    )
+  }
+}
+
--- service-worker-web.js
+++ service-worker-web.js
@@ -1,4 +1,4 @@
-var CacheName = 'moodo-cache-1527403681287',
+var CacheName = 'moodo-cache-1527414530090',
     CacheNameCommon = 'moodo-cache-data';
 
 function notifyClient(text)
@@ -22,15 +22,15 @@ self.addEventListener('install', function (e)
         {
             return cache.addAll([
                 '/web/',
-                '/web/index-1527403681287.html',
-                '/web/js/vendor-1527403681287.js',
-                '/web/js/delayedUI-1527403681287.js',
-                '/web/js/dimport-1527403681287.js',
-                '/web/js/main-min-1527403681287.js',
-                '/web/js/preload-min-1527403681287.js',
-                '/web/css/app-min-1527403681287.css',
-                '/web/css/fonts/fonticons-1527403681287.woff',
-                '/web/css/fonts/fonticons-1527403681287.ttf'
+                '/web/index-1527414530090.html',
+                '/web/js/vendor-1527414530090.js',
+                '/web/js/delayedUI-1527414530090.js',
+                '/web/js/dimport-1527414530090.js',
+                '/web/js/main-min-1527414530090.js',
+                '/web/js/preload-min-1527414530090.js',
+                '/web/css/app-min-1527414530090.css',
+                '/web/css/fonts/fonticons-1527414530090.woff',
+                '/web/css/fonts/fonticons-1527414530090.ttf'
             ]);
         }).then(caches.open(CacheNameCommon).then(function (cacheCommon)
         {
@@ -73,7 +73,7 @@ self.addEventListener('fetch', function (event)
     {
         if (urlObj.pathname === pathname)
         {
-            url = url.replace(pathname, pathname + 'index-1527403681287.html');
+            url = url.replace(pathname, pathname + 'index-1527414530090.html');
         }
 
         event.respondWith(
